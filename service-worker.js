ef93852a7e67779af28f8376ab251a3b2c3aac62
Anton <office.sklyarov@gmail.com>
2020-10-29 21:54:55 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.16ed9f4e53ea46032b77c2d8adc47978.js"
 );
 
 self.addEventListener('message', (event) => {
